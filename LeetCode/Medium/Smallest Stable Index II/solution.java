3d52a8caaaac04e98138437d75255abd0e94dda7
Anuradha Tiwari <anuradhatiwari2401@gmail.com>
2026-09-05 14:19:30 +0530

    [Syncode] LeetCode/Medium/Smallest Stable Index II

---
+++ LeetCode/Medium/Smallest Stable Index II/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] smaller = new int[n];
+        int[] larger = new int[n];
+
+        // larger[0] = nums[0];
+        int maxval = nums[0];
+        for(int i = 0; i < n; i++){
+            maxval = Math.max(nums[i], maxval);
+            larger[i] = maxval;
+            
+        }
+
+        // smaller[n-1] = nums[n-1];
+        int minval = nums[n-1];
+        for(int i = n-1; i >= 0; i--){
+            minval = Math.min(nums[i], minval);
+            smaller[i] = minval;
+
+        }
+        for(int i = 0; i < n; i++){
+            if((larger[i] - smaller[i]) <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
